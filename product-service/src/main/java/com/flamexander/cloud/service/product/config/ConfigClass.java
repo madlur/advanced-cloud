903f9b6f99a1f33c9353b7a36ed9f9d8c4b480c0
madlur <2185430@gmail.com>
2022-01-30 19:58:47 +0300

    ДЗ по уроку №5

---
+++ product-service/src/main/java/com/flamexander/cloud/service/product/config/ConfigClass.java
@@ -0,0 +1,16 @@
+package com.flamexander.cloud.service.product.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ConfigClass {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate () {
+        return new RestTemplate();
+    }
+}
